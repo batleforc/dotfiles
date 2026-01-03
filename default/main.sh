6d2318990be4f219282a44dc135d61c07b0e4662
Max Batleforc <maxleriche.60@gmail.com>
2026-01-03 17:47:59 +0100

    feat: add default in chezmoi

---
+++ default/main.sh
@@ -0,0 +1,5 @@
+source ~/.local/share/chezmoi/default/alias.sh
+source ~/.local/share/chezmoi/default/functions.sh
+source ~/.local/share/chezmoi/default/history.sh
+source ~/.local/share/chezmoi/default/init.sh
+source ~/.local/share/chezmoi/default/completion.sh
