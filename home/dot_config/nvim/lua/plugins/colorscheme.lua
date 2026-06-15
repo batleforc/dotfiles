98491dbcec3420643b7730c2e81b554a9f9a12dd
Max Batleforc <maxleriche.60@gmail.com>
2026-06-15 23:24:36 +0200

    feat: wip

--- home/dot_config/nvim/lua/plugins/colorscheme.lua
+++ home/dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 -- Kanagawa theme (https://github.com/rebelot/kanagawa.nvim)
 return {
-  { "rebelot/kanagawa.nvim", lazy = false, priority = 1000, theme = "dragon" },
+  { "rebelot/kanagawa.nvim", lazy = false, priority = 1000 },
 
   -- Tell LazyVim to use it
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "kanagawa-dragon",
     },
   },
 }
